0262e820c3ffb23e5610859e9e5ef2c103d017c4
Darnes1958 <abdel_ati_m@yahoo.com>
2025-09-12 20:10:54 +0200

    any

---
+++ app/Filament/Pages/ShowVisitor.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Filament\Pages;
+
+use App\Models\Visitor;
+use Filament\Pages\Page;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Concerns\InteractsWithTable;
+use Filament\Tables\Contracts\HasTable;
+use Filament\Tables\Table;
+
+class ShowVisitor extends Page implements HasTable
+{
+    use InteractsWithTable;
+    protected string $view = 'filament.pages.show-visitor';
+
+    public function table(Table $table): Table
+    {
+        return $table
+            ->query(function (){
+                return Visitor::query()->orderByDesc('created_at');
+            })
+            ->columns([
+                TextColumn::make('ip'),
+                TextColumn::make('user_agent'),
+                TextColumn::make('browser'),
+                TextColumn::make('platform'),
+                TextColumn::make('device'),
+                TextColumn::make('countryName')->searchable()->sortable(),
+                TextColumn::make('cityName')->searchable()->sortable(),
+                TextColumn::make('created_at'),
+            ]);
+    }
+}
+
+
