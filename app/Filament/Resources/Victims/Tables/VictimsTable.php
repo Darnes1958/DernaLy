4a25a936fb4024d52ac099f45cb6e6981799f020
Darnes1958 <abdel_ati_m@yahoo.com>
2025-09-12 11:22:03 +0200

    any

--- app/Filament/Resources/Victims/Tables/VictimsTable.php
+++ app/Filament/Resources/Victims/Tables/VictimsTable.php
@@ -2,15 +2,21 @@
 
 namespace App\Filament\Resources\Victims\Tables;
 
+
+use App\Models\Victim;
+use Filament\Actions\Action;
 use Filament\Actions\BulkActionGroup;
 use Filament\Actions\DeleteBulkAction;
 use Filament\Actions\EditAction;
 use Filament\Actions\ViewAction;
+use Filament\Forms\Components\TextInput;
+
 use Filament\Tables\Columns\IconColumn;
 use Filament\Tables\Columns\ImageColumn;
 use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Table;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Auth;
 
 class VictimsTable
 {
@@ -31,6 +37,41 @@ class VictimsTable
                 ImageColumn::make('image2')
                     ->stacked()
                     ->circular(),
+                Action::make('delete')
+                    ->iconButton()
+                    ->visible(Auth::user()->can('delete victim'))
+                    ->icon('heroicon-s-trash')
+                    ->requiresConfirmation()
+                    ->action(function (Victim $record){
+                        $record->delete();
+                    }),
+                Action::make('arc')
+                    ->iconButton()
+                    ->visible(Auth::user()->can('delete victim'))
+                    ->icon('heroicon-s-archive-box')
+                    ->requiresConfirmation()
+                    ->modalHeading('نقل السجل للأرشيف')
+                    ->modalDescription('هل انت متأكد من نقل السجل الي الأرشيف ؟')
+                    ->fillForm(fn (Victim $record): array => [
+                        'notes' => $record->notes,
+                    ])
+                    ->form([
+                        TextInput::make('notes')
+                            ->label('ملاحظات')
+                    ])
+                    ->action(function (Victim $record,array $data){
+
+                        $record=Victim::find($record->id);
+                        $oldRecord= $record;
+                        $newRecord = $oldRecord->replicate();
+
+                        $newRecord->setTable('archifs');
+                        $newRecord->id=$record->id;
+                        $newRecord->notes=$data['notes'];
+                        $newRecord->save();
+                        $record->delete();
+
+                    }),
             ])
             ->defaultSort('created_at','desc')
             ->filters([
