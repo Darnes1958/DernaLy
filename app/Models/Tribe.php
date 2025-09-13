1205dd22782496ae0e5946244bfaf2e12adbb46b
Darnes1958 <abdel_ati_m@yahoo.com>
2025-09-13 22:48:16 +0200

    any

--- app/Models/Tribe.php
+++ app/Models/Tribe.php
@@ -4,10 +4,12 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Spatie\Translatable\HasTranslations;
 
 class Tribe extends Model
 {
-    use HasFactory;
+    use HasTranslations;
+    public array $translatable = ['TriNameJs',];
 
     public function families(){
       return $this->hasMany(Family::class);
