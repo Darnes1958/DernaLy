0743c570ffd728191babf905e93f0b2082704dc3
unknown <bdel_ati_m@yahoo.com>
2025-09-28 14:28:59 +0200

    any

--- app/Filament/Forms/Components/RichEditor/RichContentCustomBlocks/HeroBlock.php
+++ app/Filament/Forms/Components/RichEditor/RichContentCustomBlocks/HeroBlock.php
@@ -3,11 +3,14 @@
 namespace App\Filament\Forms\Components\RichEditor\RichContentCustomBlocks;
 
 use Filament\Actions\Action;
-use Filament\Forms\Components\FileUpload;
-use Filament\Forms\Components\RichEditor;
+
+use Filament\Forms\Components\Hidden;
 use Filament\Forms\Components\RichEditor\RichContentCustomBlock;
-use Filament\Forms\Components\Textarea;
+
 use Filament\Forms\Components\TextInput;
+use Filament\Schemas\Components\Utilities\Get;
+use Filament\Schemas\Components\Utilities\Set;
+use Illuminate\Support\Str;
 
 class HeroBlock extends RichContentCustomBlock
 {
@@ -26,34 +29,49 @@ class HeroBlock extends RichContentCustomBlock
         return $action
             ->modalDescription('Configure the hero block')
             ->schema([
-                TextInput::make('heading')
-                    ->required(),
-                TextInput::make('subheading'),
-                RichEditor::make('details'),
-                FileUpload::make('image')
-                    ->image()
-                    ->directory('blogs'),
+                TextInput::make('url')
+                    ->label('YouTube URL')
+                    ->placeholder('e.g. https://www.youtube.com/watch?v=dQw4w9WgXcQ')
+                    ->required()
+                    ->url()
+                    ->live(onBlur: true)
+                    ->afterStateUpdated(function (Set $set, Get $get) {
+                        // Extract and store just the video ID
+                        $url = $get('url');
+                        $videoId = Str::of($url)->after('v=')->before('&');
+                        $set('videoId', $videoId);
+                    }),
+                TextInput::make('videoId')
+                    ->label('Video ID')
+
+                    ,
 
             ]);
     }
 
     public static function toPreviewHtml(array $config): string
     {
-        return view('filament.forms.components.rich-editor.rich-content-custom-blocks.hero.preview', [
-            'heading' => $config['heading'],
-            'subheading' => $config['subheading'] ?? 'Default subheading',
-            'details' => $config['details'] ?? 'Default subheading',
-            'image' => $config['image'] ?? 'Image',
-        ])->render();
+        $videoId = $config['videoId'];
+
+        return <<<HTML
+            <div class="video-container">
+                <iframe width="560" height="315" src="https://www.youtube.com/embed/{$videoId}"
+                    frameborder="0" allowfullscreen>
+                </iframe>
+            </div>
+        HTML;
     }
 
     public static function toHtml(array $config, array $data): string
     {
-        return view('filament.forms.components.rich-editor.rich-content-custom-blocks.hero.index', [
-            'heading' => $config['heading'],
-            'subheading' => $config['subheading'] ?? 'Default subheading',
-            'details' => $config['details'] ?? 'Default subheading',
-            'image' => $config['image'] ?? 'Image',
-        ])->render();
-    }
+
+        $videoId = $config['videoId'];
+
+        return <<<HTML
+            <div class="video-container">
+                <iframe width="560" height="315" src="https://www.youtube.com/embed/{$videoId}"
+                    frameborder="0" allowfullscreen>
+                </iframe>
+            </div>
+        HTML;    }
 }
