fcca8c52cb2845bc2530aac8f0991dfea34698ab
Darnes1958 <abdel_ati_m@yahoo.com>
2025-10-02 21:59:29 +0200

    good

--- resources/views/filament/forms/components/rich-editor/rich-content-custom-blocks/long-and-image/index.blade.php
+++ resources/views/filament/forms/components/rich-editor/rich-content-custom-blocks/long-and-image/index.blade.php
@@ -1,6 +1,6 @@
 <div>
     <div class="flex w-full gap-4">
-        <div class="w-10/12">
+        <div class="w-8/12">
             <div class="fi-prose">
                 {!! \Filament\Forms\Components\RichEditor\RichContentRenderer::make($theText)->customBlocks([
                         \App\Filament\Forms\Components\RichEditor\RichContentCustomBlocks\HeroBlock::class,
@@ -9,9 +9,9 @@
                         \App\Filament\Forms\Components\RichEditor\RichContentCustomBlocks\LongAndImage::class,
                     ])->toHtml() !!}
             </div>
-          
+
         </div>
-        <div class="w-2/12">
+        <div class="w-4/12">
             <figure class="w-full">
                 <img class="h-auto max-w-full rounded-lg" src="{{ asset('images/'.$theImage) }}" >
                 <figcaption class="mt-2 text-sm text-center text-gray-500 dark:text-gray-400">{{$theLabel}}</figcaption>
--- resources/views/filament/forms/components/rich-editor/rich-content-custom-blocks/long-and-image/preview.blade.php
+++ resources/views/filament/forms/components/rich-editor/rich-content-custom-blocks/long-and-image/preview.blade.php
@@ -1,9 +1,9 @@
 <div>
     <div class="flex w-full gap-4">
-        <div class="w-10/12">
+        <div class="w-8/12">
             {{$theText}}
         </div>
-        <div class="w-2/12">
+        <div class="w-4/12">
             <figure class="w-full">
                 <img class="h-auto max-w-full rounded-lg" src="{{ asset('images/'.$theImage) }}" >
                 <figcaption class="mt-2 text-sm text-center text-gray-500 dark:text-gray-400">{{$theLabel}}</figcaption>
