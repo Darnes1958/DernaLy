444b3e638e3917d6ab8ebc8680e3c0b747076b65
unknown <bdel_ati_m@yahoo.com>
2025-10-06 09:47:21 +0200

    any

--- resources/views/filament/forms/components/rich-editor/rich-content-custom-blocks/long-and-image/index.blade.php
+++ resources/views/filament/forms/components/rich-editor/rich-content-custom-blocks/long-and-image/index.blade.php
@@ -1,6 +1,6 @@
 <div>
     <div class="flex w-full gap-4">
-        <div class="w-{{$w1}}/12">
+        <div class="w-full sm:w-{{$w1}}/12">
             <div class="fi-prose">
                 {!! \Filament\Forms\Components\RichEditor\RichContentRenderer::make($theText)->customBlocks([
                         \App\Filament\Forms\Components\RichEditor\RichContentCustomBlocks\HeroBlock::class,
@@ -11,7 +11,7 @@
             </div>
 
         </div>
-        <div class="w-{{$w2}}/12">
+        <div class="w-full sm:w-{{$w2}}/12">
             @php if ($imgHeight!='auto') $imgHeight=$imgHeight.'pt' @endphp
             @php if ($imgWidth!='auto') $imgWidth=$imgWidth.'pt' @endphp
             <figure class="w-full">
