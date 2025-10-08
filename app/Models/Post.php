36ff4b11c9094a0c10f837c78bad376ceaa37199
unknown <bdel_ati_m@yahoo.com>
2025-10-08 11:39:04 +0200

    any

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -3,8 +3,16 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Post extends Model
 {
-    //
+    public function Author(): BelongsTo
+    {
+        return $this->belongsTo(Author::class);
+    }
+    public function Category(): BelongsTo
+    {
+        return $this->belongsTo(Category::class);
+    }
 }
